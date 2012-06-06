57a14ddd4f99ae669062d4700d435e8d35480c50
James Broad <jamesbroad@jamesbroad.(none)>
2012-06-06 17:52:31 +0100

    Adding node.js dummy app

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@ require 'rspec/expectations'
 # require 'debugger'
 
 Capybara.run_server = false
-Capybara.app_host = 'http://localhost:9000'
+Capybara.app_host = 'http://localhost:3000'
 Capybara.default_selector = :css 
 
 Capybara.default_driver = :mechanize
